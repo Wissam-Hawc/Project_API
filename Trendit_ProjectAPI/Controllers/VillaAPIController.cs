14dc59416b377f0de7fe6628efc0a686be32755b
Wissam-Hawc <wissam.hawcharw@gmail.com>
2024-06-11 12:33:11 +0300

    implemented the ApiResponse in villaController and add try catch block

--- Trendit_ProjectAPI/Controllers/VillaAPIController.cs
+++ Trendit_ProjectAPI/Controllers/VillaAPIController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Trendit_ProjectAPI.Data;
+using System.Net;
 using Trendit_ProjectAPI.Models;
 using Trendit_ProjectAPI.Models.Dto;
 using Trendit_ProjectAPI.Repository.IRepository;
@@ -14,12 +14,14 @@ namespace Trendit_ProjectAPI.Controllers
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
+        protected APIResponse _response;
         private readonly IVillaRepository _dbVilla;
         private readonly IMapper _mapper;
         public VillaAPIController(IVillaRepository dbVilla, IMapper mapper)
         {
             _dbVilla = dbVilla;
             _mapper = mapper;
+            this._response = new();
         }
 
 
@@ -27,29 +29,50 @@ namespace Trendit_ProjectAPI.Controllers
         // if not specified it will be HttpGet by default 
         [ProducesResponseType(StatusCodes.Status200OK)]
 
-        public async Task<ActionResult<IEnumerable<VillaDTO>>> getVillas()
+        public async Task<ActionResult<APIResponse>> getVillas()
         {
-            IEnumerable<Villa> villaList = await _dbVilla.GetAllAsync();
-            return Ok(_mapper.Map<List<VillaDTO>>(villaList));
+            try
+            {
+                IEnumerable<Villa> villaList = await _dbVilla.GetAllAsync();
+                _response.Result = _mapper.Map<List<VillaDTO>>(villaList);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
         [HttpGet("id", Name = "GetVilla")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         //[ProducesResponseType(200, Type = typeof(VillaDTO))]
-        public async Task<ActionResult<VillaDTO>> getVilla(int id)
+        public async Task<ActionResult<APIResponse>> GetVilla(int id)
         {
-            if (id == 0)
+            try
             {
-                return BadRequest();
+                if (id == 0)
+                {
+                    return BadRequest();
+                }
+                var villa = await _dbVilla.GetAsync(villa => villa.Id == id);
+                if (villa == null)
+                {
+                    return NotFound();
+                }
+                _response.Result = _mapper.Map<VillaDTO>(villa);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
             }
-            var villa = await _dbVilla.GetAsync(villa => villa.Id == id);
-            if (villa == null)
+            catch (Exception ex)
             {
-                return NotFound();
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
             }
-
-            return Ok(_mapper.Map<VillaDTO>(villa));
+            return _response;
             //FirstOrDefault is a linq methode to get the first element that match or the default null
         }
 
@@ -57,25 +80,35 @@ namespace Trendit_ProjectAPI.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<VillaDTO>> CreateVilla([FromBody] VillaCreateDTO createDTO)
+        public async Task<ActionResult<APIResponse>> CreateVilla([FromBody] VillaCreateDTO createDTO)
         {
-            if (await _dbVilla.GetAsync(villa => villa.Name.ToLower() == createDTO.Name.ToLower()) != null)
-            /*here will get the first villa in villastore who s name equal to the one send, if no result it will
-            return null so no duplication names 
-            */
+            try
             {
-                ModelState.AddModelError("CustomError", "Villa Already Exist");
-                return BadRequest(ModelState);
+                if (await _dbVilla.GetAsync(villa => villa.Name.ToLower() == createDTO.Name.ToLower()) != null)
+                /*here will get the first villa in villastore who s name equal to the one send, if no result it will
+                return null so no duplication names 
+                */
+                {
+                    ModelState.AddModelError("CustomError", "Villa Already Exist");
+                    return BadRequest(ModelState);
+                }
+                if (createDTO == null)
+                {
+                    return BadRequest(createDTO);
+                }
+                Villa villa = _mapper.Map<Villa>(createDTO);
+                await _dbVilla.CreateAsync(villa);
+                _response.Result = _mapper.Map<VillaDTO>(villa);
+                _response.StatusCode = HttpStatusCode.Created;
+                return CreatedAtRoute("GetVilla", new { id = villa.Id }, _response);
+                //the id because the getVilla route needs an id also 
             }
-            if (createDTO == null)
+            catch (Exception ex)
             {
-                return BadRequest(createDTO);
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
             }
-            Villa model= _mapper.Map<Villa>(createDTO);
-            await _dbVilla.CreateAsync(model);
-
-            return CreatedAtRoute("GetVilla", new { id = model.Id }, model);
-            //the id because the getVilla route needs an id also 
+            return _response;
 
         }
 
@@ -84,33 +117,55 @@ namespace Trendit_ProjectAPI.Controllers
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> DeleteVilla(int id)
+        public async Task<ActionResult<APIResponse>> DeleteVilla(int id)
         //IactionResult because we will not return a type 
         {
-            if (id == 0)
+            try
             {
-                return BadRequest();
+                if (id == 0)
+                {
+                    return BadRequest();
+                }
+                var villa = await _dbVilla.GetAsync(villa => villa.Id == id);
+                if (villa == null)
+                {
+                    return NotFound();
+                }
+                await _dbVilla.RemoveAsync(villa);
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
             }
-            var villa =await _dbVilla.GetAsync(villa => villa.Id == id);
-            if (villa == null)
+            catch (Exception ex)
             {
-                return NotFound();
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
             }
-            await _dbVilla.RemoveAsync(villa);
-            return NoContent();
+            return _response;
         }
 
         [HttpPut("id", Name = "UpdateVilla")]
-        public async Task<IActionResult> UpdateVilla(int id, [FromBody] VillaUpdateDTO updateDTO)
+        public async Task<ActionResult<APIResponse>> UpdateVilla(int id, [FromBody] VillaUpdateDTO updateDTO)
         {
-            if (updateDTO == null || id != updateDTO.Id)
+            try
             {
-                return BadRequest();
-            }
-            Villa model = _mapper.Map<Villa>(updateDTO);
-            await _dbVilla.UpdateAsync(model);
+                if (updateDTO == null || id != updateDTO.Id)
+                {
+                    return BadRequest();
+                }
+                Villa model = _mapper.Map<Villa>(updateDTO);
+                await _dbVilla.UpdateAsync(model);
 
-            return NoContent();
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
 
         [HttpPatch("id", Name = "UpdatePartialVilla")]
@@ -121,7 +176,7 @@ namespace Trendit_ProjectAPI.Controllers
                 return BadRequest();
             }
             //tracked false because we need no tracking so there will be no error by tracking 2 same time
-            var villa = await _dbVilla.GetAsync(villa => villa.Id == id,tracked:false);
+            var villa = await _dbVilla.GetAsync(villa => villa.Id == id, tracked: false);
             VillaUpdateDTO villaDTO = _mapper.Map<VillaUpdateDTO>(villa);
             if (villa == null)
             {
--- Trendit_ProjectAPI/Models/APIResponse.cs
+++ Trendit_ProjectAPI/Models/APIResponse.cs
@@ -5,6 +5,7 @@ namespace Trendit_ProjectAPI.Models
     public class APIResponse
     {
         public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } = true;
         public List<string> ErrorMessages { get; set; }
         public object Result { get; set; }
     }
