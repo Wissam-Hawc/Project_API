629bc8bed74825f1d238fb6f403d912713a1696b
Wissam-Hawc <wissam.hawcharw@gmail.com>
2024-06-11 16:52:50 +0300

    Add base services for  basic functionality for sending htpp requests.

---
+++ Trendit_Project_Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using Trendit_Project_Web.Models;
+
+namespace Trendit_Project_Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest apiRequest);
+    }
+}
