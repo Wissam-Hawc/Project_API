e5b65650ac595a3e4fdf11237f874809c99099d1
Wissam-Hawc <wissam.hawcharw@gmail.com>
2024-06-11 15:11:03 +0300

    add a class library for apiType and add apiRequest class

---
+++ Trendit_Project_Utility/SD.cs
@@ -0,0 +1,14 @@
+﻿namespace Trendit_Project_Utility
+{
+    public static class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+
+    }
+}
---
+++ Trendit_Project_Web/Models/APIRequest.cs
@@ -0,0 +1,11 @@
+﻿using static Trendit_Project_Utility.SD;
+
+namespace Trendit_Project_Web.Models
+{
+    public class APIRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+    }
+}
